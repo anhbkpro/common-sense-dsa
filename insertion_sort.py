fa6b2f8678bac8b7fafbc4938bdcfff4b123cb4c
Andy <laingocanh1990@gmail.com>
2021-03-20 20:38:33 +0700

    shift left value to the right if the value at position is larger than temp_value

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,13 +1,24 @@
 def insertion_sort(array):
-    for i in range(1, len(array)):
-        temp_value = array[i]
-        print(f"Pass-though #{i}: temp_value={temp_value}")
-        # if i - 1 >= 0 and array[i-1] > temp_value:
-        #     array[i] = array[i-1]
-        for j in range(i):
-            if i-j-1 >= 0 and array[i-j-1] > temp_value:
-                array[i-j], array[i-j-1] = array[i-j-1], array[i-j]
-                print(f'after switching now the array is {array}')
+    '''
+    This version does not swap 2 values, just shift left value to the right if the value at position is larger than the temp_value  
+    '''
+    for index in range(1, len(array)):
+        temp_value = array[index]
+        position = index - 1
+
+        print(f"Pass-through #{index}: temp_value={temp_value}")
+        while position >= 0:
+            # check whether the value at position is greater than the temp_value
+            if array[position] > temp_value:
+                # we shift that left value to the right
+                array[position + 1] = array[position]
+                position = position - 1
+            else:
+                break  # it’s time to move the temp_value into the gap
+        # The final step of each pass-through is moving the temp_value into the gap
+        array[position + 1] = temp_value
+        print(f'after switching now the array is {array}')
+    return array
 
 
 print(insertion_sort([4, 2, 7, 1, 3]))
