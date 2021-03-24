14f7845281a3bd6a4aa3fdda5496008dfc300369
AnhLai <laingocanh1990@gmail.com>
2021-03-24 23:07:56 +0700

    added a default argument of index=0

--- recursive/double_array.py
+++ recursive/double_array.py
@@ -1,12 +1,12 @@
-def double_array(array, index):
-	array[index] *= 2
-	if index < len(array) - 1:
-		index += 1
-		double_array(array, index)
+def double_array(array, index=0):
+    # Base case: when the index goes past the end of the array
+    if index >= len(array):
+        return
+
+    array[index]*=2
+    double_array(array, index + 1)
 
 
 array = [1, 2, 3, 4]
-
 double_array(array, 0)
-
 print(array)
