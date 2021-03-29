461d51243c865652c1840900b49ea92696d3b455
AnhLai <laingocanh1990@gmail.com>
2021-03-29 21:12:55 +0700

    format code

--- recursive/arraysum.py
+++ recursive/arraysum.py
@@ -1,22 +1,22 @@
 def arraysum(array, index=0):
-	if index == len(array):
-		return 0
-	return array[index] + arraysum(array[index + 1 : len(array)])
+    if index == len(array):
+        return 0
+    return array[index] + arraysum(array[index + 1: len(array)])
 
-    
+
 def arraysum1(array):
-	if len(array) == 1:
-		return array[0]
-	return array[0] + arraysum1(array[1 : len(array)])
+    if len(array) == 1:
+        return array[0]
+    return array[0] + arraysum1(array[1: len(array)])
 
-    
+
 def reverse(text):
     if len(text) == 1:
         return text[0]
-    return reverse(text[1 : len(text)]) + text[0]
+    return reverse(text[1: len(text)]) + text[0]
 
-    
+
 def countX(text):
     if len(text) == 0:
         return 0
-    return countX(text[1 : len(text)]) + (1 if text[0] == 'x' else 0)
+    return countX(text[1: len(text)]) + (1 if text[0] == 'x' else 0)
--- recursive/character_count.py
+++ recursive/character_count.py
@@ -1,8 +1,8 @@
 def character_count(array):
     # Base case: when the array is empty:
-	if len(array) == 0:
-		return 0
-	return len(array[0]) + character_count(array[1:])
+    if len(array) == 0:
+        return 0
+    return len(array[0]) + character_count(array[1:])
 
 
-character_count( ["ab", "c", "def", "ghij"])
+character_count(["ab", "c", "def", "ghij"])
