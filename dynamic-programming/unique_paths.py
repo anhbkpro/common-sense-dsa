848750a5315319e1bb5eb6c37bc70cbe22263a45
AnhLai <laingocanh1990@gmail.com>
2021-03-29 23:17:16 +0700

    refactor code

--- dynamic-programming/unique_paths.py
+++ dynamic-programming/unique_paths.py
@@ -1,9 +1,10 @@
 def unique_paths(rows, columns, memo={}):
 	if rows == 1 or columns == 1:
 		return 1
-	if f'{rows}{columns}' not in memo:
-		memo[f'{rows}{columns}'] = unique_paths(rows - 1, columns, memo) + unique_paths(rows, columns - 1, memo)
-	return memo[f'{rows}{columns}']
+	key = f'{rows}{columns}'
+	if key not in memo:
+		memo[key] = unique_paths(rows - 1, columns, memo) + unique_paths(rows, columns - 1, memo)
+	return memo[key]
 
 
 print(unique_paths(6, 7, {}))
