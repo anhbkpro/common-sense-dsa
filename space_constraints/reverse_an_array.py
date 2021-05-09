9f4890d4833293b92af202f4fbfafb489db0d9b7
AnhLai <laingocanh1990@gmail.com>
2021-05-09 21:23:38 +0700

    more concise code

--- space_constraints/reverse_an_array.py
+++ space_constraints/reverse_an_array.py
@@ -1,11 +1,6 @@
 def reverse(array):
-	array_length = len(array) - 1
+	length = len(array) - 1
 	for index, item in enumerate(array):
-		if index < array_length/2:
-			temp = item
-			item = array[array_length-index]
-			print(item)
-			array[array_length-index] = temp
-			array[index] = item
-			print(f'assign {item} to item at {index}')
-			print(array[index])
+		if index < length/2:
+			array[index], array[length - index] = array[length - index], array[index]
+
