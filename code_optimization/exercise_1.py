8966e0dc8aa430908513cac1fa3e0714609b5330
AnhLai <laingocanh1990@gmail.com>
2021-05-21 10:40:17 +0700

    write a function that accepts two arrays of players and returns an array of the players who play in both sports

---
+++ code_optimization/exercise_1.py
@@ -0,0 +1,39 @@
+basketball_players = [
+    {"first_name": "Jill", "last_name": "Huang", "team": "Gators"},
+    {"first_name": "Janko", "last_name": "Barton", "team": "Sharks"},
+    {"first_name": "Wanda", "last_name": "Vakulskas", "team": "Sharks"},
+    {"first_name": "Jill", "last_name": "Moloney", "team": "Gators"},
+    {"first_name": "Luuk", "last_name": "Watkins", "team": "Gators"}
+]
+
+football_players = [
+    {"first_name": "Hanzla", "last_name": "Radosti", "team": "32ers"},
+    {"first_name": "Tina", "last_name": "Watkins", "team": "Barleycorns"},
+    {"first_name": "Alex", "last_name": "Patel", "team": "32ers"},
+    {"first_name": "Jill", "last_name": "Huang", "team": "Barleycorns"},
+    {"first_name": "Wanda", "last_name": "Vakulskas", "team": "Barleycorns"}
+]
+
+basketball_player_hash = {}
+for player in basketball_players:
+    player_name = player['first_name'] + ' ' + player['last_name']
+    if player_name not in basketball_player_hash:
+        basketball_player_hash[player_name] = player['team']
+
+print(basketball_player_hash)
+
+
+football_player_hash = {}
+for player in football_players:
+    player_name = player['first_name'] + ' ' + player['last_name']
+    if player_name not in football_player_hash:
+        football_player_hash[player_name] = player['team']
+
+print(football_player_hash)
+
+result = []
+for item in basketball_player_hash:
+    if item in football_player_hash:
+        result.append(item)
+
+print(result)
